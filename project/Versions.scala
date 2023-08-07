925246d5d3f755f4e62c44f268477741d385076d
takapi327 <t.takapi0327@gmail.com>
2023-08-07 16:20:14 +0900

    Create Versions

---
+++ project/Versions.scala
@@ -0,0 +1,17 @@
+/*
+ * This file is part of the IxiaS service.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+object ScalaVersions {
+  val scala212 = "2.12.11"
+  val scala213 = "2.13.18"
+  val scala3 = "3.3.0"
+}
+
+object JavaVersions {
+  val java8 = "8"
+  val java11 = "11"
+}
