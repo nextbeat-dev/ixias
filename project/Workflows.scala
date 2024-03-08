168af5a22500887c8d2850fa53c48ffc8f6a0aa7
takapi327 <t.takapi0327@gmail.com>
2024-03-08 14:33:46 +0900

    Add settingsSns workflow

--- project/Workflows.scala
+++ project/Workflows.scala
@@ -1,6 +1,16 @@
 import sbtghactions._
 
 object Workflows {
+
+  val settingsSns: WorkflowStep.Run = WorkflowStep.Run(
+    commands = List(
+      "aws sns create-topic --name testSNS --endpoint-url http://localhost:4566 --region ap-northeast-1",
+      "aws sns subscribe --topic-arn arn:aws:sns:ap-northeast-1:000000000000:testSNS --protocol lambda --notification-endpoint arn:aws:lambda:ap-northeast-1:000000000000:function:testFunction --endpoint-url http://localhost:4566 --region ap-northeast-1"
+    ),
+    env = Map("AWS_ACCESS_KEY_ID" -> "dummy", "AWS_SECRET_ACCESS_KEY" -> "dummy", "AWS_DEFAULT_REGION" -> "ap-northeast-1"),
+    name = Some("Create SNS topic")
+  )
+
   val dockerRun: WorkflowStep.Run = WorkflowStep.Run(
     commands = List("docker compose up -d"),
     name     = Some("Start up LocalStack on Docker")
