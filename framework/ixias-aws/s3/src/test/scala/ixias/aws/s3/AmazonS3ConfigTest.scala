06a6dfba03151042efa874509e913d364fbfeca0
takapi327 <t.takapi0327@gmail.com>
2024-03-08 14:47:34 +0900

    Create AmazonS3ConfigTest

---
+++ framework/ixias-aws/s3/src/test/scala/ixias/aws/s3/AmazonS3ConfigTest.scala
@@ -0,0 +1,31 @@
+package ixias.aws.s3
+
+import scala.util.{ Success, Failure }
+
+import munit.FunSuite
+
+import com.amazonaws.regions.Regions
+
+import ixias.aws.DataSourceName
+
+class AmazonS3ConfigTest extends FunSuite with AmazonS3Config {
+
+  implicit val dsn: DataSourceName = DataSourceName("aws.s3://dummy_bucket")
+
+  // --[ Test ]-------------------------------------------------------------------
+  test("AmazonS3Config#getAWSAccessKeyId") {
+    assertEquals(getAWSAccessKeyId, Some("dummy"))
+  }
+
+  test("AmazonS3Config#getAWSSecretKey") {
+    assertEquals(getAWSSecretKey, Some("dummy"))
+  }
+
+  test("AmazonS3Config#getAWSCredentials") {
+    assertEquals(getAWSCredentials.nonEmpty, true)
+  }
+
+  test("AmazonS3Config#getAWSRegion") {
+    assertEquals(getAWSRegion, Success(Regions.AP_NORTHEAST_1))
+  }
+}
