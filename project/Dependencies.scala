ca8d567675d914d82d06612db27ff3588c32a765
takapi327 <t.takapi0327@gmail.com>
2024-03-08 10:41:14 +0900

    Add shapeless3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,6 +15,7 @@ object Dependencies {
   val connectorJava = "com.mysql" % "mysql-connector-j" % "8.3.0"
 
   val shapeless = "com.chuusai" %% "shapeless" % "2.3.10"
+  val shapeless3 = "org.typelevel" %% "shapeless3-deriving" % "3.4.0"
 
   val typesafeConfig = "com.typesafe" % "config" % "1.4.3"
 
