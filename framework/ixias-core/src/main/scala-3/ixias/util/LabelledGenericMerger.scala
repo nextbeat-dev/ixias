296badc56014bca5bcd358c68341e5d192cc9b82
takapi327 <t.takapi0327@gmail.com>
2025-02-17 20:14:06 +0900

    Fixed compile

--- framework/ixias-core/src/main/scala-3/ixias/util/LabelledGenericMerger.scala
+++ framework/ixias-core/src/main/scala-3/ixias/util/LabelledGenericMerger.scala
@@ -16,30 +16,33 @@ trait LabelledGenericMerger[T, U]:
   def merge(data1: T, data2: U, hard: Boolean = false): T
 
 object LabelledGenericMerger:
+
+  private case class Impl[T, U]()(using mT: Mirror.ProductOf[T], mU: Mirror.ProductOf[U])
+    extends LabelledGenericMerger[T, U]:
+    def merge(data1: T, data2: U, hard: Boolean): T =
+      val mergedValues = productMerge(
+        productToList(data1),
+        productToList(data2),
+        hard
+      )
+      val productAsTuple = listToTuple[Tuple](mergedValues)
+      mT.fromProduct(productAsTuple)
+
+    private def productToList[A](a: A)(using m: Mirror.ProductOf[A]) =
+      a.asInstanceOf[Product].productIterator.toList
+
+    private def productMerge(a: List[Any], b: List[Any], hard: Boolean): List[Any] =
+      a.zipAll(b, None, None).map {
+        case (left, right) =>
+          (left, right) match
+            case (Some(lv), Some(rv)) => if hard then Some(rv) else Some(rv)
+            case (None, Some(rv))     => Some(rv)
+            case (Some(lv), None)     => if hard then None else Some(lv)
+            case _                    => if hard then right else if right != null then right else left
+      }
+
+    private def listToTuple[T](list: List[Any]): T =
+      Tuple.fromArray(list.toArray).asInstanceOf[T]
+
   inline def apply[T, U](using mT: Mirror.ProductOf[T], mU: Mirror.ProductOf[U]): LabelledGenericMerger[T, U] =
-    new LabelledGenericMerger[T, U]:
-
-      def merge(data1: T, data2: U, hard: Boolean): T =
-        val mergedValues = productMerge(
-          productToList(data1),
-          productToList(data2),
-          hard
-        )
-        val productAsTuple = listToTuple[Tuple](mergedValues)
-        mT.fromProduct(productAsTuple)
-
-      private def productToList[A](a: A)(using m: Mirror.ProductOf[A]) =
-        a.asInstanceOf[Product].productIterator.toList
-
-      private def productMerge(a: List[Any], b: List[Any], hard: Boolean): List[Any] =
-        a.zipAll(b, None, None).map {
-          case (left, right) =>
-            (left, right) match
-              case (Some(lv), Some(rv)) => if hard then Some(rv) else Some(rv)
-              case (None, Some(rv))     => Some(rv)
-              case (Some(lv), None)     => if hard then None else Some(lv)
-              case _                    => if hard then right else if right != null then right else left
-        }
-
-      private def listToTuple[T](list: List[Any]): T =
-        Tuple.fromArray(list.toArray).asInstanceOf[T]
+    Impl[T, U]()
